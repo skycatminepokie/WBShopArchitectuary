b5df87b45330abda562bcc853427f74e265c9bc0
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2022-09-01 21:34:36 -0700

    Set up command registration block

--- common/src/main/java/skycat/wbshop/WorldborderShop.java
+++ common/src/main/java/skycat/wbshop/WorldborderShop.java
@@ -1,9 +1,18 @@
 package skycat.wbshop;
 
+import com.google.common.base.Suppliers;
+import dev.architectury.event.events.common.CommandRegistrationEvent;
+import dev.architectury.registry.registries.Registries;
+
+import java.util.function.Supplier;
+
 public class WorldborderShop {
     public static final String MOD_ID = "wbshop";
+    public static final Supplier<Registries> REGISTRIES = Suppliers.memoize(() -> Registries.get(MOD_ID));
 
     public static void init() {
-
+        CommandRegistrationEvent.EVENT.register(((dispatcher, selection) -> {
+            // Register commands here
+        }));
     }
 }
